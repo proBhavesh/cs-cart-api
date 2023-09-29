af5f4f4f2d992ccaab5032723e61886537a650ce
Bhavesh <63704323+proBhavesh@users.noreply.github.com>
2023-09-30 02:22:18 +0530

    commiting changes from dell

--- client/api/test.py
+++ client/api/test.py
@@ -15,3 +15,25 @@ auth_service = AuthService(session_store, admin_email, admin_api_key)
 # Use AuthService to send authentication requests
 print(auth_service.send_auth_request("vendor@example.com"))
 # print(auth_service.send_auth_request("vendor2@example.com"))
+
+# # Import GetPagesService class from GetPagesService.py
+# from getpages import GetPagesService
+
+# # Initialize GetPagesService with admin email and API key
+# get_pages_service = GetPagesService(
+#     "vendor@example.com", "1T5W3C21870R410Y47c814Rck3l2v215"
+# )
+
+# # Define parameters for the GET request
+# params = {
+#     "page": 1,
+#     "items_per_page": 10,
+#     "sort_by": "position",
+#     "sort_order": "desc",
+# }
+
+# # Send request to get pages with parameters
+# response = get_pages_service.send_pages_request(params)
+
+# # Print the response
+# print(response)
