78c2a646b114009425ef4178365663dc3e2bdbff
Shubham <forevershubham123@gmail.com>
2023-10-22 22:28:44 +0530

    products,stores,vendors,orders update

---
+++ src/app/backend/api/orders/get_orders.py
@@ -0,0 +1,90 @@
+import os
+import base64
+import requests
+import json
+from dotenv import load_dotenv
+
+from global_data import (
+    BASE_URL,
+)
+
+
+class OrdersService:
+    def __init__(self, email, api_key, url=BASE_URL):
+        if not email or not api_key:
+            raise ValueError("Email and API key must be set")
+        self.credentials = base64.b64encode(
+            f"{email}:{api_key}".encode()).decode()
+        self.url = url + "/api/orders/"
+
+    def get_orders(self, params={}):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Basic {self.credentials}",
+        }
+        try:
+            response = requests.get(self.url, headers=headers, params=params)
+            return self._handle_response(response)
+        except requests.exceptions.RequestException as e:
+            return {"Error": str(e)}
+
+    def get_order(self, order_id):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Basic {self.credentials}",
+        }
+        try:
+            response = requests.get(self.url + str(order_id), headers=headers)
+            return self._handle_response(response)
+        except requests.exceptions.RequestException as e:
+            return {"Error": str(e)}
+
+    def create_order(self, order_data):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Basic {self.credentials}",
+        }
+        try:
+            response = requests.post(
+                self.url, headers=headers, json=order_data)
+            return self._handle_response(response)
+        except requests.exceptions.RequestException as e:
+            return {"Error": str(e)}
+
+    def update_order(self, order_id, order_data):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Basic {self.credentials}",
+        }
+        try:
+            response = requests.put(
+                self.url + str(order_id), headers=headers, json=order_data
+            )
+            return self._handle_response(response)
+        except requests.exceptions.RequestException as e:
+            return {"Error": str(e)}
+
+    def delete_order(self, order_id):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Basic {self.credentials}",
+        }
+        try:
+            response = requests.delete(
+                self.url + str(order_id), headers=headers)
+            return response.status_code
+        except requests.exceptions.RequestException as e:
+            return {"Error": str(e)}
+
+    def _handle_response(self, response):
+        if response.status_code in [200, 201, 204]:
+            try:
+                json_response = response.json()
+                print(json_response)
+                return json_response
+            except json.JSONDecodeError:
+                raise ValueError("Response from server was not valid JSON")
+        else:
+            raise ConnectionError(
+                f"Request failed with status code {response.status_code}"
+            )
--- tests/test_orders.py
+++ tests/test_orders.py
@@ -1,5 +1,5 @@
 # example usage
-from api.shipments import OrderService
+from api.orders import OrdersService
 import os
 from dotenv import load_dotenv
 import json
@@ -9,10 +9,66 @@ load_dotenv()
 vendor_email = os.getenv("VENDOR_EMAIL")
 vendor_api_key = os.getenv("VENDOR_API_KEY")
 
+print(vendor_api_key, vendor_email)
+
 # Initialize SessionStore and AuthService
-product_service = OrderService(vendor_email, vendor_api_key)
+orders_service = OrdersService(vendor_email, vendor_api_key)
+
+
+# json_response = orders_service.get_orders()
+# json_response = orders_service.get_order(order_id=1)
+
+# Create an order
+order_data = {
+    "user_id": "0",
+    "payment_id": "2",
+    "shipping_id": "1",
+    "products": {
+        "1": {
+            "product_id": "12",
+            "amount": "1"
+        },
+        "2": {
+           "product_id": "13",
+           "amount":"2"
+        }
+    },
+    "user_data": {
+        "email": "guest@example.com",
+        "firstname": "Guest",
+        "lastname": "Guest",
+        "s_firstname": "Guest",
+        "s_lastname": "Guest",
+        "s_country": "US",
+        "s_city": "Boston",
+        "s_state": "MA",
+        "s_zipcode": "02125",
+        "s_address": "44 Main street",
+        "b_firstname": "Guest",
+        "b_lastname": "Guest",
+        "b_country": "US",
+        "b_city": "Boston",
+        "b_state": "MA",
+        "b_zipcode": "02125",
+        "b_address": "44 Main street"
+    }
+}
+json_response =  orders_service.create_order(order_data)
+
+# Update an order
+update_data = {
+   "status": "P",
+    "user_data": {
+        "email": "newemail@example.com",
+    },
+    "total": "100"
+}
+# json_response =  orders_service.update_order(order_id=1, order_data=update_data)
 
 # Use AuthService to send authentication requests
-json_response = product_service.send_auth_request()
+# json_response = orders_service.send_auth_request()
+
+
+
 print(json.dumps(json_response, indent=4))
 # print(auth_service.send_auth_request("vendor2@example.com"))
