19204dedd424aa2505e900f1418fecf064223ec6
Shubham <forevershubham123@gmail.com>
2023-10-23 01:40:56 +0530

    apps done

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -37,3 +37,51 @@ print(auth_service.send_auth_request("vendor@example.com"))
 
 # # Print the response
 # print(response)
+
+
+# List language variables
+lang_code = "en"  # substitute with the required language code
+print(auth_service.get_langvars(lang_code))
+
+# Get specific language variable
+name = "test_variable"  # substitute with the required variable name
+print(auth_service.get_langvar(name, lang_code))
+
+# Create a new language variable
+new_name = "new_test_var"
+new_value = "This is a new test variable."
+print(auth_service.create_langvar(new_name, new_value, lang_code))
+
+# Update a language variable
+updated_value = "This is an updated value for the variable."
+print(auth_service.update_langvar(name, updated_value, lang_code))
+
+# Delete a language variable
+delete_name = "to_be_deleted_var"  # substitute with the variable to be deleted
+print(auth_service.delete_langvar(delete_name))
+
+
+# List installed languages
+print(auth_service.get_languages())
+
+# Get specific language
+lang_id = 1  # Substitute with required language ID
+print(auth_service.get_language(lang_id))
+
+# Create a new language
+lang_code = "ts"
+name = "New Language"
+status = "A"
+country_code = "US"
+print(auth_service.create_language(lang_code, name, status, country_code))
+
+# Update a language
+updated_name = "Updated Language"
+updated_status = "H"
+updated_country_code = "RU"
+print(auth_service.update_language(lang_id, lang_code,
+      updated_name, updated_status, updated_country_code))
+
+# Delete a language
+delete_lang_id = 3  # Substitute with the ID of the language to be deleted
+print(auth_service.delete_language(delete_lang_id))
