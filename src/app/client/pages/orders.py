b809e9ca61b6d938b5922ebd9c9de25d89996b98
ShubhamJiaswal7 <you@example.com>
2023-10-29 03:06:26 +0530

    update for ui

--- src/app/client/pages/orders.py
+++ src/app/client/pages/orders.py
@@ -1,3 +1,80 @@
 import streamlit as st
+import pandas as pd
 
-st.write(f"You have selected orders")
+# Create a Streamlit app for the Order Page
+st.title('Orders')
+col1, col2, col3 = st.columns(3)
+col1.metric("Total Orders", "567", "Order Dashboard")
+col2.metric("Total Order Value", "Rs 12345", "Order Dashboard")
+
+# Define a function to open the modal for adding an order
+def create_order_modal():
+    with st.form(key='create_order_form'):
+        st.header('Place a New Order')
+
+        # Add input fields for order information
+        product_name = st.text_input('Product Name')
+        quantity = st.number_input('Quantity', min_value=1)
+        customer_name = st.text_input('Customer Name')
+        order_description = st.text_area('Order Description')
+
+        # Add a submit button
+        submit_button = st.form_submit_button('Place Order')
+
+        if submit_button:
+            # You can process the order data here and add it to your data source
+            # For example, you can add it to a DataFrame or send it to a database
+
+            # Assuming you have a data source called 'orders_data' as a list of dictionaries
+            new_order = {
+                'Product Name': product_name,
+                'Quantity': quantity,
+                'Customer Name': customer_name,
+                'Order Description': order_description,
+                'Order ID': len(orders_data) + 1
+            }
+            orders_data.append(new_order)
+
+            # Clear the form fields
+            st.text_input('Product Name', value='')
+            st.number_input('Quantity', min_value=1)
+            st.text_input('Customer Name', value='')
+            st.text_area('Order Description', value='')
+
+# Check if the 'Place Order' button is clicked
+if st.button('Place Order'):
+    create_order_modal()
+
+# Display the order information table
+st.title('Order Information Table')
+
+# Define the order data as a list of dictionaries
+orders_data = [
+    {
+        'Product Name': 'Product A',
+        'Quantity': 2,
+        'Customer Name': 'Customer A',
+        'Order Description': 'Description A',
+        'Order ID': 1
+    },
+    {
+        'Product Name': 'Product B',
+        'Quantity': 3,
+        'Customer Name': 'Customer B',
+        'Order Description': 'Description B',
+        'Order ID': 2
+    },
+    {
+        'Product Name': 'Product C',
+        'Quantity': 1,
+        'Customer Name': 'Customer C',
+        'Order Description': 'Description C',
+        'Order ID': 3
+    }
+]
+
+# Create a DataFrame from the order data
+df = pd.DataFrame(orders_data)
+
+# Display the table using st.dataframe
+st.dataframe(df, width=1000, height=600)
--- src/app/client/pages/vendors.py
+++ src/app/client/pages/vendors.py
@@ -1,3 +1,79 @@
 import streamlit as st
+import pandas as pd
 
-st.write(f"You have selected vendors")
+# Create a Streamlit app for the Vendor Page
+st.title('Vendor Management')
+col1, col2, col3 = st.columns(3)
+col1.metric("Total Vendors", "50", "Vendor Dashboard")
+
+# Define a function to open the modal for adding a vendor
+def create_vendor_modal():
+    with st.form(key='create_vendor_form'):
+        st.header('Add a New Vendor')
+
+        # Add input fields for vendor information
+        vendor_name = st.text_input('Vendor Name')
+        vendor_contact = st.text_input('Contact Information')
+        vendor_address = st.text_input('Address')
+        vendor_description = st.text_area('Vendor Description')
+
+        # Add a submit button
+        submit_button = st.form_submit_button('Add Vendor')
+
+        if submit_button:
+            # You can process the vendor data here and add it to your data source
+            # For example, you can add it to a DataFrame or send it to a database
+
+            # Assuming you have a data source called 'vendors_data' as a list of dictionaries
+            new_vendor = {
+                'Vendor Name': vendor_name,
+                'Contact Information': vendor_contact,
+                'Address': vendor_address,
+                'Vendor Description': vendor_description,
+                'Vendor ID': len(vendors_data) + 1
+            }
+            vendors_data.append(new_vendor)
+
+            # Clear the form fields
+            st.text_input('Vendor Name', value='')
+            st.text_input('Contact Information', value='')
+            st.text_input('Address', value='')
+            st.text_area('Vendor Description', value='')
+
+# Check if the 'Add Vendor' button is clicked
+if st.button('Add Vendor'):
+    create_vendor_modal()
+
+# Display the vendor information table
+st.title('Vendor Information Table')
+
+# Define the vendor data as a list of dictionaries
+vendors_data = [
+    {
+        'Vendor Name': 'Vendor A',
+        'Contact Information': 'vendora@example.com',
+        'Address': '123 Main St, City, Country',
+        'Vendor Description': 'Description A',
+        'Vendor ID': 1
+    },
+    {
+        'Vendor Name': 'Vendor B',
+        'Contact Information': 'vendorb@example.com',
+        'Address': '456 Elm St, City, Country',
+        'Vendor Description': 'Description B',
+        'Vendor ID': 2
+    },
+    {
+        'Vendor Name': 'Vendor C',
+        'Contact Information': 'vendorc@example.com',
+        'Address': '789 Oak St, City, Country',
+        'Vendor Description': 'Description C',
+        'Vendor ID': 3
+    }
+]
+
+# Create a DataFrame from the vendor data
+df = pd.DataFrame(vendors_data)
+
+# Display the table using st.dataframe
+st.dataframe(df, width=1000, height=len(df) * 200)
