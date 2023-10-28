ef634f112e8d6c82cd8d0a326bea899da3e3a0f9
Bhavesh <63704323+proBhavesh@users.noreply.github.com>
2023-10-29 03:06:26 +0530

    update for ui

--- src/app/client/pages/product.py
+++ src/app/client/pages/product.py
@@ -1,3 +1,79 @@
 import streamlit as st
+import pandas as pd
 
-st.write(f"You have selected product")
+# Create a Streamlit app
+st.title('Products')
+col1, col2, col3 = st.columns(3)
+col1.metric("Total Products", "2345", "Store Dashboard")
+col2.metric("Total Sales", "Rs 2345", "Sales Dashboard")
+# Define a function to open the modal for adding a product
+def create_product_modal():
+    with st.form(key='create_product_form'):
+        st.header('Add a New Product')
+
+        # Add input fields for product information
+        product_name = st.text_input('Product Name')
+        product_cost = st.text_input('Product Cost')
+        product_image = st.file_uploader('Product Image', type=['jpg', 'png', 'jpeg'])
+        product_description = st.text_area('Product Description')
+
+        # Add a submit button
+        submit_button = st.form_submit_button('Add Product')
+
+        if submit_button:
+            # You can process the product data here and add it to your data source
+            # For example, you can add it to a DataFrame or send it to a database
+
+            # Assuming you have a data source called 'products_data' as a list of dictionaries
+            new_product = {
+                'Product Name': product_name,
+                'Product Cost': product_cost,
+                'Product Image': product_image,
+                'Product Description': product_description,
+                'Product ID': len(products_data) + 1
+            }
+            products_data.append(new_product)
+
+            # Clear the form fields
+            st.text_input('Product Name', value='')
+            st.text_input('Product Cost', value='')
+            st.file_uploader('Product Image', type=['jpg', 'png', 'jpeg'])
+            st.text_area('Product Description', value='')
+
+# Check if the 'Add Product' button is clicked
+if st.button('Add Product'):
+    create_product_modal()
+
+# Display the product information table
+st.title('Product Information Table')
+
+# Define the product data as a list of dictionaries
+products_data = [
+    {
+        'Product Name': 'Product A',
+        'Product Cost': 'Rs 100',
+        'Product Image': None,
+        'Product Description': 'Description A',
+        'Product ID': 1
+    },
+    {
+        'Product Name': 'Product B',
+        'Product Cost': 'Rs 200',
+        'Product Image': None,
+        'Product Description': 'Description B',
+        'Product ID': 2
+    },
+    {
+        'Product Name': 'Product C',
+        'Product Cost': 'Rs 150',
+        'Product Image': None,
+        'Product Description': 'Description C',
+        'Product ID': 3
+    }
+]
+
+# Create a DataFrame from the product data
+df = pd.DataFrame(products_data)
+
+# Display the table using st.dataframe
+st.dataframe(df, width=1000, height=600)
--- src/app/client/pages/stores.py
+++ src/app/client/pages/stores.py
@@ -1,3 +1,86 @@
 import streamlit as st
+import pandas as pd
+import requests
+from tests.test_stores import store_service
 
-st.write(f"You have selected stores")
+st.title('Stores')
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Total stores", "2345", "Store Dashboard")
+col2.metric("Total Sales", "Rs 2345", "Sales Dashboard")
+#col3.metric("Humidity", "86%", "4%")
+
+
+
+if st.button('Create Store'):
+    # Create a modal for creating a new store
+    with st.form(key='create_store_form'):
+        st.header('Create a New Store')
+
+        # Add input fields for store name and description
+        store_name = st.text_input('Store Name')
+        store_email = st.text_input('Store Email')
+        store_description = st.text_area('Store Description')
+
+        # Add a submit button
+        submit_button = st.form_submit_button('Create Store')
+
+    if submit_button:
+        # Define the store data
+        store_data = {
+            "company": store_name,
+            "storefront": store_email,
+            "secure_storefront": store_description,
+            "clone_from": "2",
+            "clone": {
+                "layouts": "i",
+                "settings": "Yi",
+                "products": "Yi",
+                "categories": "iY"
+            }
+        }
+
+        # Define the API endpoint for creating a store
+        create_store_url = "http://localhost:8501/test/test_stores"  # Replace with the actual URL
+
+        # Send a POST request to create the store
+        response = requests.post(create_store_url, json=store_data)
+
+        # Check if the request was successful (HTTP status code 200)
+        if response.status_code == 200:
+            json_response = response.json()  # Parse the JSON response
+            st.success(f'Store created successfully. Store Name: {store_name}')
+        else:
+            st.error('Failed to create the store. Please check the server response.')
+
+
+# data = {
+#     'SL Number': [1, 2, 3, 4, 5],
+#     'Store Name': ['Store A', 'Store B', 'Store C', 'Store D', 'Store E'],
+#     'Created Date': ['2023-10-01', '2023-10-02', '2023-10-03', '2023-10-04', '2023-10-05'],
+#     'Store Description': ['Description A', 'Description B', 'Description C', 'Description D', 'Description E']
+# }
+json_response = store_service.get_stores()
+
+# Create a DataFrame from the sample data
+df = pd.DataFrame(json_response)
+
+# Create a Streamlit app
+st.title('Store Information Table')
+
+# Add Edit and Delete buttons with icons
+# edit_icon = "https://image.flaticon.com/icons/svg/565/565026.svg"
+# delete_icon = "https://image.flaticon.com/icons/svg/3221/3221897.svg"
+
+df['Actions'] = [
+    f"[Edit]| [Delete]"
+    for _ in range(len(df))
+]
+
+# Display the table using st.dataframe
+st.dataframe(df, width=800, height=400)
+
+
+
+
+
