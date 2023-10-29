84ea1a63d545209cacde68a743e3f8dc0718c3e0
Shubham <forevershubham123@gmail.com>
2023-10-29 06:29:48 +0530

    get API Integrated for all Screens issue in user

--- src/app/client/pages/stores.py
+++ src/app/client/pages/stores.py
@@ -1,15 +1,33 @@
 import streamlit as st
 import pandas as pd
 import requests
-from tests.test_stores import store_service
+from api.stores import StoresService
+import os
+from dotenv import load_dotenv
+import json
+
+# Load environment variables
+load_dotenv()
+vendor_email = os.getenv("VENDOR_EMAIL")
+vendor_api_key = os.getenv("VENDOR_API_KEY")
+
+print(vendor_api_key, vendor_email)
+
+# Initialize Session and StoresService
+store_service = StoresService(vendor_email, vendor_api_key)
+
+# Fetch all stores
+json_response = store_service.get_stores()
+
+stores = pd.json_normalize(json_response['stores'])
+stores.index = range(1, len(stores) + 1)
 
 st.title('Stores')
 
 col1, col2, col3 = st.columns(3)
-col1.metric("Total stores", "2345", "Store Dashboard")
+col1.metric("Total stores", len(stores), "Store Dashboard")
 col2.metric("Total Sales", "Rs 2345", "Sales Dashboard")
-#col3.metric("Humidity", "86%", "4%")
-
+# col3.metric("Humidity", "86%", "4%")
 
 
 if st.button('Create Store'):
@@ -41,7 +59,8 @@ if st.button('Create Store'):
         }
 
         # Define the API endpoint for creating a store
-        create_store_url = "http://localhost:8501/test/test_stores"  # Replace with the actual URL
+        # Replace with the actual URL
+        create_store_url = "http://localhost:8501/test/test_stores"
 
         # Send a POST request to create the store
         response = requests.post(create_store_url, json=store_data)
@@ -51,7 +70,8 @@ if st.button('Create Store'):
             json_response = response.json()  # Parse the JSON response
             st.success(f'Store created successfully. Store Name: {store_name}')
         else:
-            st.error('Failed to create the store. Please check the server response.')
+            st.error(
+                'Failed to create the store. Please check the server response.')
 
 
 # data = {
@@ -60,10 +80,7 @@ if st.button('Create Store'):
 #     'Created Date': ['2023-10-01', '2023-10-02', '2023-10-03', '2023-10-04', '2023-10-05'],
 #     'Store Description': ['Description A', 'Description B', 'Description C', 'Description D', 'Description E']
 # }
-json_response = store_service.get_stores()
 
-# Create a DataFrame from the sample data
-df = pd.DataFrame(json_response)
 
 # Create a Streamlit app
 st.title('Store Information Table')
@@ -72,15 +89,7 @@ st.title('Store Information Table')
 # edit_icon = "https://image.flaticon.com/icons/svg/565/565026.svg"
 # delete_icon = "https://image.flaticon.com/icons/svg/3221/3221897.svg"
 
-df['Actions'] = [
-    f"[Edit]| [Delete]"
-    for _ in range(len(df))
-]
+stores['Actions'] = [f"[Edit]| [Delete]" for _ in range(len(stores))]
 
 # Display the table using st.dataframe
-st.dataframe(df, width=800, height=400)
-
-
-
-
-
+st.dataframe(stores, width=800, height=len(stores))
