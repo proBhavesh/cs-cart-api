ea7f70dd5d089325af57157c5555fe4c8b48a0cc
Bhavesh <63704323+proBhavesh@users.noreply.github.com>
2024-02-02 21:30:01 +0530

    removed manual url concatenation

--- src/app/backend/api/api_key_generation/generate_api_key.py
+++ src/app/backend/api/api_key_generation/generate_api_key.py
@@ -1,5 +1,6 @@
 from base64 import b64encode
 from typing import Any, Dict, Optional
+from urllib.parse import urljoin
 import requests
 import json
 
@@ -7,13 +8,12 @@ from api import BASE_URL, encode_credentials
 
 
 class APIKeyGeneratorService:
-    def __init__(self, admin_email: str, admin_api_key: str, url: Optional[str] = None):
+    def __init__(self, admin_email: str, admin_api_key: str):
         if not admin_email or not admin_api_key:
             raise ValueError("Admin email and API key must be set")
 
         self.credentials = encode_credentials(admin_email, admin_api_key)
-
-        self.url = url if url else BASE_URL + "/api/generateAPIKey"
+        self.url = urljoin(BASE_URL, "/api/generateAPIKey")
 
     def generate_api_key(self, vendor_email: str) -> Dict:
         headers = {
@@ -24,7 +24,6 @@ class APIKeyGeneratorService:
 
         try:
             response = requests.post(url=self.url, headers=headers, data=payload)
-            # TODO print(response)
             return self._handle_response(response)
         except requests.exceptions.RequestException as e:
             return {"Error": str(e)}
@@ -34,10 +33,7 @@ class APIKeyGeneratorService:
             try:
                 json_response = response.json()
                 return json_response
-                # TODO print(json_response)
             except json.JSONDecodeError:
                 raise ValueError("Response from server was not a valid JSON")
         else:
-            raise ConnectionError(
-                f"Request failed with status code {response.status_code}"
-            )
+            raise ConnectionError(f"Request failed with status code {response.status_code}")
--- src/app/backend/api/shipments/orders.py
+++ src/app/backend/api/shipments/orders.py
@@ -5,18 +5,12 @@ from api import BASE_URL, encode_credentials
 
 
 class OrderService:
-    # The URL parameter is now optional and defaults to None
-    def __init__(
-        self,
-        vendor_email: str,
-        vendor_api_key: str,
-        url: Optional[str] = None,
-    ):
+    def __init__(self, vendor_email: str, vendor_api_key: str):
         if not vendor_email or not vendor_api_key:
             raise ValueError("Vendor email and API key must be set")
 
         self.credentials = encode_credentials(vendor_email, vendor_api_key)
-        self.url = url if url else BASE_URL
+        self.url = BASE_URL
 
     def send_auth_request(self) -> Any | Dict[str, str]:
         headers = {
@@ -25,7 +19,7 @@ class OrderService:
         }
 
         try:
-            response = requests.get(self.url, headers=headers)
+            response = requests.get(url=self.url, headers=headers)
             return self._handle_response(response)
         except requests.exceptions.RequestException as e:
             return {"Error": str(e)}
@@ -39,6 +33,4 @@ class OrderService:
             except json.JSONDecodeError:
                 raise ValueError("Response from server was not a valid JSON")
         else:
-            raise ConnectionError(
-                f"Request failed with status code {response.status_code}"
-            )
+            raise ConnectionError(f"Request failed with status code {response.status_code}")
