7266f62db0c4e658587ea67ff63d5285774f0281
Bhavesh <63704323+proBhavesh@users.noreply.github.com>
2024-02-06 20:59:10 +0530

    updated ui code with session states

--- src/app/client/app.py
+++ src/app/client/app.py
@@ -29,23 +29,31 @@ kinde_api_client_params = {
 
 kinde_client = KindeApiClient(**kinde_api_client_params)
 
+# a = kinde_client.get_login_url()
+
+a = kinde_client.is_authenticated();
+
+print(a)
+
+
+# http://localhost:8501/?code=olKeN-9JyTJpV8OD25WONmwI5HhwUp5_Rb3BY8XrzYs.sqwX_Z_dhCYaNA8HabzcrFgb0JvMHyFRdXBCCqvztbU&scope=openid%20profile%20email%20offline&state=wauk1eXEQ7q242u2nwo0Z2XgWi8hqm
 # Streamlit UI
-st.title('Streamlit App with Kinde Authentication')
+# st.title('Streamlit App with Kinde Authentication')
 
-if 'access_token' not in st.session_state:
-    st.session_state['access_token'] = None
+# if 'access_token' not in st.session_state:
+#     st.session_state['access_token'] = None
 
-if st.session_state['access_token'] is None:
-    login_url = kinde_client.get_login_url()
-    st.sidebar.markdown(f"[Login with Kinde]({login_url})")
-else:
-    user_details = kinde_client.get_user_details()
-    print("this is user details",user_details)
-    user_email = user_details.get('email', 'No email available')
-    st.sidebar.write(f"Welcome {user_details['given_name']} {user_details['family_name']}")
-    st.sidebar.write(f"Email: {user_email}")
+# if st.session_state['access_token'] is None:
+#     login_url = kinde_client.get_login_url()
+#     st.sidebar.markdown(f"[Login with Kinde]({login_url})")
+# else:
+#     user_details = kinde_client.get_user_details()
+#     print("this is user details",user_details)
+#     user_email = user_details.get('email', 'No email available')
+#     st.sidebar.write(f"Welcome {user_details['given_name']} {user_details['family_name']}")
+#     st.sidebar.write(f"Email: {user_email}")
 
-    if st.sidebar.button('Logout'):
-        logout_url = kinde_client.logout(redirect_to=KINDE_POST_LOGOUT_REDIRECT_URL)
-        st.session_state['access_token'] = None
-        st.experimental_rerun()
+#     if st.sidebar.button('Logout'):
+#         logout_url = kinde_client.logout(redirect_to=KINDE_POST_LOGOUT_REDIRECT_URL)
+#         st.session_state['access_token'] = None
+#         st.experimental_rerun()
