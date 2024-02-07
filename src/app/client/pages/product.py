c7199d724ed84c96b5e62bd91813707e48099d44
Bhavesh <63704323+proBhavesh@users.noreply.github.com>
2024-02-07 12:39:46 +0530

    removed un-necessary log statements

--- src/app/client/pages/product.py
+++ src/app/client/pages/product.py
@@ -13,27 +13,26 @@ product_service = ProductsService(vendor_email, vendor_api_key)
 
 
 # Create a Streamlit app
-st.title('Products')
+st.title("Products")
 
 # Define a function to open the modal for adding a product
 
 
 def create_product_modal():
     print("Entered create_product_modal function")
-    with st.form(key='create_product_form'):
+    with st.form(key="create_product_form"):
         print("Inside st.form")
-        submit_button = st.form_submit_button('Add Product')
+        submit_button = st.form_submit_button("Add Product")
 
         if submit_button:
             print("Form submitted")
-    print('Exited create_product_modal function')
+    print("Exited create_product_modal function")
 
 
 json_response = product_service.get_products()
 # Convert your JSON response to a DataFrame
-products = pd.json_normalize(json_response['products'])
+products = pd.json_normalize(json_response["products"])
 products.index = range(1, len(products) + 1)
-print(json_response)
 
 
 col1, col2, col3 = st.columns(3)
@@ -42,14 +41,14 @@ col2.metric("Total Sales", "Rs 2345", "Sales Dashboard")
 
 
 # Check if the 'Add Product' button is clicked
-if st.button('Add Product'):
+if st.button("Add Product"):
     print("Add Product button clicked")
     create_product_modal()
     print("create_product_modal function call has finished")
 
 # Display the product information table
-st.title('Product Information Table')
+st.title("Product Information Table")
 
 
 # Display table using st.dataframe
-st.dataframe(products, width=1200, height=len(products)*2)
+st.dataframe(products, width=1200, height=len(products) * 2)
