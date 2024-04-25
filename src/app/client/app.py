e3996eef08029752183acd49c516431733275720
Bhavesh <63704323+proBhavesh@users.noreply.github.com>
2024-04-25 18:15:34 +0530

    Refactor authentication logic and improve cookie handling in app.py

--- src/app/client/app.py
+++ src/app/client/app.py
@@ -3,18 +3,14 @@ from streamlit_oauth import OAuth2Component
 import os
 import base64
 import json
-import extra_streamlit_components as stx
 from dotenv import load_dotenv
+from streamlit_cookies_controller import CookieController
+
 
 from utils.helpers import generateAPIKey
 
 load_dotenv()
-
-cookie_manager = stx.CookieManager(key="outer_cookie")
-if cookie_manager.get(cookie="email"):
-    email = cookie_manager.get(cookie="email")
-    st.write(email)
-
+controller = CookieController()
 
 # create an OAuth2Component instance
 CLIENT_ID = os.getenv("KINDE_CLIENT_ID")
@@ -25,14 +21,20 @@ TOKEN_ENDPOINT = f"https://{KINDE_DOMAIN}/oauth2/token"
 REVOKE_ENDPOINT = f"https://{KINDE_DOMAIN}/oauth2/revoke"
 KINDE_REDIRECT_URI = os.getenv("KINDE_REDIRECT_URI")
 
-if "auth" not in st.session_state:
-    # cookie_manager = stx.CookieManager()
+if controller.get("email"):
+    st.write("You are already logged in!")
+    controller.set("email", controller.get("email"))
+    controller.set("token", controller.get("token"))
+    st.write(controller.get("email"))
+    if st.button("Logout"):
+        controller.remove("email")
+        controller.remove("token")
+        st.experimental_rerun()
+else:
     st.title("KINDE OIDC Example")
     st.write(
-        "This example shows how to use the raw OAuth2 component to authenticate with a Kinde OAuth2 ."
+        "This example shows how to use the raw OAuth2 component to authenticate with a Kinde OAuth2."
     )
-
-    # create a button to start the OAuth2 flow
     oauth2 = OAuth2Component(
         CLIENT_ID,
         CLIENT_SECRET,
@@ -51,28 +53,13 @@ if "auth" not in st.session_state:
     )
 
     if result:
-        st.write(result)
-        # decode the id_token jwt and get the user's email address
+        # Decode the ID token to get the user's email address
         id_token = result["token"]["id_token"]
-        # verify the signature is an optional step for security
         payload = id_token.split(".")[1]
-        # add padding to the payload if needed
-        payload += "=" * (-len(payload) % 4)
-        payload = json.loads(base64.b64decode(payload))
-        # print(payload)
-        email = payload["email"]
-        st.session_state["auth"] = email
-        st.session_state["token"] = result["token"]
-        st.rerun()
-else:
-    cookie_manager = stx.CookieManager(key="inner_cookie")
-    st.write("You are logged in!")
-    st.write(st.session_state["auth"])
-    st.write(st.session_state["token"])
-    # print(st.session_state["token"])
-    # print(generateAPIKey(st.session_state["auth"]))
-    cookie_manager.set("email", st.session_state["auth"], key="email")
-    cookie_manager.set("token", st.session_state["token"], key="token_auth")
-    if st.button("Logout"):
-        del st.session_state["auth"]
-        del st.session_state["token"]
+        payload += "=" * (-len(payload) % 4)  # Correct padding for Base64 decoding
+        email = json.loads(base64.b64decode(payload)).get("email")
+        # Set session state and cookies
+        controller.set("email", email)
+        controller.set("token", result["token"])
+
+        st.experimental_rerun()
